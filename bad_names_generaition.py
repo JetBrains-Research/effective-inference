f1b700e5f24c20a107012c9993204e020dac998d
Sasha Fedorova <sasha@ip-172-31-43-210.eu-west-1.compute.internal>
2023-12-08 00:24:03 +0000

    added generate_and_save function, logging changed

--- bad_names_generaition.py
+++ bad_names_generaition.py
@@ -14,6 +14,7 @@ logging.basicConfig(filename='/home/sasha/effective-inference/logs/accurasies.lo
 
 
 
+
 tokenizer = CodeLlamaTokenizer.from_pretrained("codellama/CodeLlama-7b-hf")
 model = LlamaForCausalLM.from_pretrained("codellama/CodeLlama-7b-hf", load_in_8bit=True,
     device_map="auto")
@@ -22,10 +23,23 @@ code_data = pd.read_csv('code_data.csv')
 
 df = pd.DataFrame(columns = ['name_type', 'prompt', 'real', 'generated', 'answer'])
 st = time.time()
+acc_dict = {'all': 0, 'Original':0, 'GPT generated':0, 'Numerical': 0}
 
-# logging.info('')
+def generate_and_save(prompt, code, name, type):
+    # prompt is based on huggingface example
+    PROMPT = prompt + "\n" + (name+"(").join(code.split(name+"(")[:i-1]) + "<FILL_ME>"
+    
+    # generation
+    input_ids = tokenizer(PROMPT, return_tensors="pt")["input_ids"].to('cuda')
+    generated_ids = model.generate(input_ids, max_new_tokens=10)
+    filling = tokenizer.batch_decode(generated_ids[:, input_ids.shape[1]:], skip_special_tokens = True)[0]
 
-acc_dict = {'all': 0, 'original':0, 'full_changed_names':0, 'numerical_names': 0}
+    # logging
+    if filling[:len(name)] == name:
+        acc_dict[type]+=1
+    df.loc[len(df)] = {'name_type':type, 'prompt':PROMPT, 'real': (name+"(")+(name+"(").join(code.split(name+"(")[i-1:]), 'generated':filling, 'answer': filling[:len(name)] == name} 
+
+
 for j in tqdm(range(2)):#code_data.shape[0]):
     ex = code_data.loc[j]
     prompt_names_dict = json.loads(ex['prompt_names_dict'])
@@ -35,53 +49,24 @@ for j in tqdm(range(2)):#code_data.shape[0]):
         for i in range(ex['prompt'].count(name+"(")):
             # print(name, bad_name, numerical_name, "\n\n")
             acc_dict['all']+=1
+
+            generate_and_save(ex['prompt'], ex['code'], name, 'Original')
             
-            PROMPT = ex['prompt'] + "\n" + (name+"(").join(ex['code'].split(name+"(")[:i-1]) + "<FILL_ME>"
-            # print("\nPrompt\n", PROMPT)
-            input_ids = tokenizer(PROMPT, return_tensors="pt")["input_ids"].to('cuda')
-            generated_ids = model.generate(input_ids, max_new_tokens=10)
-            filling = tokenizer.batch_decode(generated_ids[:, input_ids.shape[1]:], skip_special_tokens = True)[0]
-            if filling[:len(name)] == name:
-                acc_dict['original']+=1
-            df.loc[len(df)] = {'name_type':'Original', 'prompt':PROMPT, 'real': (name+"(")+(name+"(").join(ex['code'].split(name+"(")[i-1:]), 'generated':filling, 'answer': filling[:len(name)] == name} 
+            generate_and_save(ex['bad_prompt'], ex['bad_code'], bad_name, 'GPT generated')
 
-
-            BAD_PROMPT = ex['bad_prompt'] + "\n" + (bad_name+"(").join(ex['bad_code'].split(bad_name+"(")[:i-1]) + "<FILL_ME>"
-            # print("\nPrompt\n", BAD_PROMPT)
-            input_ids = tokenizer(BAD_PROMPT, return_tensors="pt")["input_ids"].to('cuda')
-            generated_ids = model.generate(input_ids, max_new_tokens=10)
-            filling = tokenizer.batch_decode(generated_ids[:, input_ids.shape[1]:], skip_special_tokens = True)[0]
-            if filling[:len(bad_name)] == bad_name:
-                acc_dict['full_changed_names']+=1
-                
+            generate_and_save(ex['numerical_prompt'], ex['numerical_code'], numerical_name, 'Numerical')
             
-            df.loc[len(df)]= {'name_type':'GPT generated','prompt':BAD_PROMPT, 'real':(bad_name+"(")+(bad_name+"(").join(ex['bad_code'].split(bad_name+"(")[i-1:]), 'generated':filling, 'answer':filling[:len(bad_name)] == bad_name}
-                
-            NUMERICAL_PROMPT = ex['numerical_prompt'] + "\n" + (numerical_name+"(").join(ex['numerical_code'].split(numerical_name+"(")[:i-1]) + "<FILL_ME>"
-            # print("\nPrompt\n",NUMERICAL_PROMPT)
-            input_ids = tokenizer(NUMERICAL_PROMPT, return_tensors="pt")["input_ids"].to('cuda')
-            generated_ids = model.generate(input_ids, max_new_tokens=10)
-            filling = tokenizer.batch_decode(generated_ids[:, input_ids.shape[1]:], skip_special_tokens = True)[0]
-            if filling[:len(numerical_name)] == numerical_name:
-                acc_dict['numerical_names']+=1
-
-            df.loc[len(df)] = {'name_type':'Numerical','prompt':NUMERICAL_PROMPT, 'real':(numerical_name+"(")+(numerical_name+"(").join(ex['numerical_code'].split(numerical_name+"(")[i-1:]), 'generated':filling, 'answer':filling[:len(numerical_name)] == numerical_name}
-
             df.to_csv(f'logs/generation_data_{st}.csv')
 
-
-print(acc_dict)
-print(f"Original functions: {acc_dict['original']/acc_dict['all']}")
 logging.info(f"---------------------------\n\n")
-
-logging.info(f"Original functions: {acc_dict['original']/acc_dict['all']}")
-logging.info(f"Original function promt example: {PROMPT}")
-print(f"GPT generated functions: {acc_dict['full_changed_names']/acc_dict['all']}")
-logging.info(f"GPT generated functions: {acc_dict['full_changed_names']/acc_dict['all']}")
-logging.info(f"GPT generated function promt example: {BAD_PROMPT}")
-print(f"Numerical functions: {acc_dict['numerical_names']/acc_dict['all']}")
-logging.info(f"Numerical functions: {acc_dict['numerical_names']/acc_dict['all']}")
-logging.info(f"Numerical function promt example: {NUMERICAL_PROMPT}")
+logging.info(f"Dataset size is: {acc_dict['all']}")
+print(acc_dict)
+print(f"Original functions: {acc_dict['Original']/acc_dict['all']}")
+logging.info(f"Original functions: {acc_dict['Original']/acc_dict['all']}")
+print(f"GPT generated functions: {acc_dict['GPT generated']/acc_dict['all']}")
+logging.info(f"GPT generated functions: {acc_dict['GPT generated']/acc_dict['all']}")
+print(f"Numerical functions: {acc_dict['Numerical']/acc_dict['all']}")
+logging.info(f"Numerical functions: {acc_dict['Numerical']/acc_dict['all']}")
 
 # Remember to close the file handler to release the resources
 logging.shutdown()
